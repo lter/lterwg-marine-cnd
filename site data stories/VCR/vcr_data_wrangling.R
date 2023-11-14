a7a8437a6299e39671586279eb0680b380bc9e47
mackwhite <mwhite@fiu.edu>
2023-11-13 23:15:53 -0500

    began process of converting lengths to weights for vcr fish data

---
+++ site data stories/VCR/vcr_data_wrangling.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(readr)
+
+vcr_dat <- read_csv("~/Downloads/vcr_2012thru2018_fish.csv")
+fish_species <- as.data.frame(unique(vcr_dat$speciesName)) |> 
+  mutate(a = NA, 
+         b = NA)
+write_csv(fish_species, "vcr_fish_species_lwr.csv")
