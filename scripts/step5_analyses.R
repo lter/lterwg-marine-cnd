1077c0528d9e61d3179b9b75684b986c7dd0ea0f
mackwhite <mwhite@fiu.edu>
2024-01-30 23:30:08 -0500

    summarizing fce data for plotting

--- scripts/step5_analyses.R
+++ scripts/step5_analyses.R
@@ -87,4 +87,53 @@ fce_wide <- fce |>
   pivot_wider(names_from = c(measurement_type, measurement_unit),
               values_from = measurement_value)
 glimpse(fce_wide)
+summary(fce_wide)
+sum(is.infinite(fce_wide$`density_num/m`))#2 infinites - going to simply remove
+fce_wide <- fce_wide[!is.infinite(fce_wide$`density_num/m`), ]
 
+fce_summ <- fce_wide |> 
+  rename(
+    density_num_m = `density_num/m`,
+    ind_drymass_g = `dmperind_g/ind`,
+    ind_n_ug_hr = `nind_ug/hr`,
+    ind_p_ug_hr = `pind_ug/hr`
+  ) |> 
+  select(
+    -project, -habitat, -raw_filename, -row_num, -subsite_level3, -species, -`density_num/m2`
+  ) |> 
+  group_by(
+    year, month, site, subsite_level1, subsite_level2
+    ) |>
+  summarize(
+    n_tot_ug_m_hr = sum(ind_n_ug_hr*density_num_m, na.rm = TRUE),
+    n_ind_sd = sd(ind_n_ug_hr, na.rm = TRUE),
+    n_ind_sd_nozeros = sd(ind_n_ug_hr[ind_n_ug_hr != 0], na.rm = TRUE),
+    n_ind_mean = mean(ind_n_ug_hr, na.rm = TRUE),
+    n_ind_mean_nozeros = mean(ind_n_ug_hr[ind_n_ug_hr != 0], na.rm = TRUE),
+    n_ind_cv = (n_ind_sd/n_ind_mean)*100,
+    n_ind_cv_nozeros = (n_ind_sd_nozeros/n_ind_mean_nozeros)*100,
+    p_tot_ug_m_hr = sum(ind_p_ug_hr*density_num_m, na.rm = TRUE),
+    p_ind_sd = sd(ind_p_ug_hr, na.rm = TRUE),
+    p_ind_sd_nozeros = sd(ind_p_ug_hr[ind_p_ug_hr != 0], na.rm = TRUE),
+    p_ind_mean = mean(ind_p_ug_hr, na.rm = TRUE),
+    p_ind_mean_nozeros = mean(ind_p_ug_hr[ind_p_ug_hr != 0], na.rm = TRUE),
+    p_ind_cv = (p_ind_sd/p_ind_mean)*100,
+    p_ind_cv_nozeros = (p_ind_sd_nozeros/p_ind_mean_nozeros)*100,
+    bm_tot_m = sum(ind_drymass_g*density_num_m),
+    bm_ind_sd = sd(ind_drymass_g, na.rm = TRUE),
+    bm_ind_sd_nozeros = sd(ind_drymass_g[ind_drymass_g != 0], na.rm = TRUE),
+    bm_ind_mean = mean(ind_drymass_g, na.rm = TRUE),
+    bm_ind_mean_nozeros = mean(ind_drymass_g[ind_drymass_g != 0], na.rm = TRUE),
+    bm_ind_cv = (bm_ind_sd/bm_ind_mean)*100,
+    bm_ind_cv_nozeros = (bm_ind_sd_nozeros/bm_ind_mean_nozeros)*100,
+    n_spp = n_distinct(scientific_name[ind_drymass_g != 0])
+    ) 
+
+fce_summ_clean <- fce_summ |> 
+  filter(!is.na(subsite_level1)) |> #removes weird NAs in subsite_level1 -still kicking around
+  filter(!(site == "TB" & year %in% c(2004:2007))) #removes years 2004 through 2007 for TB - we werent sampling then
+unique(fce_summ_clean$site)
+#seems like a lot of artifical zeros 
+#below shows legitimate zeros - need to recode columns and select() then join to link up with above and get rid of artifical zeros
+map <- mastermap_yrs1thru19 |> 
+  filter(SPECIESCODE == 13)
