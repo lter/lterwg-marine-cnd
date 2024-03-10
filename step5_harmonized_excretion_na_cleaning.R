33edb8adf44dd8eb1eb13ce61c5d898bbd23e126
mackwhite <mwhite@fiu.edu>
2024-03-09 20:04:07 -0500

    began summarizing data for plotting/analyses

---
+++ step5_harmonized_excretion_na_cleaning.R
@@ -0,0 +1,117 @@
+###project: LTER Marine Consumer Nutrient Dynamic Synthesis Working Group
+###author(s): Mack White, Li Kui, Angel Chen
+###goal(s): clean up missing data in harmonized_consumer_excretion.csv where possible
+###date(s): January - March 2024
+###note(s): 
+
+# Housekeeping ------------------------------------------------------------
+
+### load necessary libraries
+### install.packages("librarian")
+librarian::shelf(tidyverse, googledrive, readxl, taxize, stringr)
+
+### set google drive path
+exc_ids <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1VakpcnFVckAYNggv_zNyfDRfkcGTjZxX")) |> 
+  dplyr::filter(name %in% c("harmonized_consumer_excretion.csv"))
+rm(list = ls()) #cleans env
+
+### read in data from google drive
+dt <- read.csv(file.path("tier2", "harmonized_consumer_excretion.csv"),stringsAsFactors = F,na.strings =".") 
+glimpse(dt)
+
+# data manipulation -------------------------------------------------------
+
+dt_wide <- dt |> 
+  pivot_wider(names_from = c(measurement_type, measurement_unit), 
+              values_from = measurement_value) 
+glimpse(dt_wide)  
+
+### check for NAs
+na_count_per_column <- sapply(dt_wide, function(x) sum(is.na(x)))
+print(na_count_per_column) #1002 observations of NAs in dmperind_g/ind
+
+### check out NAs in dmperind_g/ind column
+NA_dmperind_g <- dt_wide |> 
+  filter(is.na(`dmperind_g/ind`))
+unique(NA_dmperind_g$project) #CCE only project
+unique(NA_dmperind_g$scientific_name) #number of species
+
+### try to find average for each species
+cce_avg_dm_species <- dt_wide |> 
+  filter(project == "CCE") |> 
+  group_by(project, scientific_name) |> 
+  summarize(avg_dmperind = mean(`dmperind_g/ind`, na.rm = TRUE),
+            n = n())
+print(cce_avg_dm_species)
+
+#Copepoda does not have any estimate...pyrsomes missing estimates for 2006-2009
+dt_wide1 <- dt_wide |> 
+  filter(!(project == "CCE" & scientific_name == "Copepoda"))
+
+NA_dmperind_g <- dt_wide1 |> 
+  filter(is.na(`dmperind_g/ind`))
+unique(NA_dmperind_g$scientific_name) #Copepoda removed :)
+
+### add species average to remainder of NAs
+cce_spp_avg <- dt_wide1 |> 
+  filter(project == "CCE", !is.na(`dmperind_g/ind`)) |> 
+  group_by(scientific_name) |> 
+  summarise(avg_dmperind = mean(`dmperind_g/ind`, na.rm = TRUE))
+
+dt_wide2 <- dt_wide1 |> 
+  left_join(cce_spp_avg, by = "scientific_name") |> 
+  mutate(`dmperind_g/ind` = if_else(project == "CCE" & is.na(`dmperind_g/ind`), avg_dmperind, `dmperind_g/ind`)) |> 
+  select(-avg_dmperind)  #remove the temporary avg_dmperind column
+
+### re-check to ensure NAs are gone
+na_count_per_column <- sapply(dt_wide2, function(x) sum(is.na(x)))
+print(na_count_per_column) #looks good :)
+
+### check on nas in subsite_level1
+NA_subsite_level1 <- dt_wide2 |> 
+  filter(is.na(subsite_level1))
+unique(NA_subsite_level1$project) #"CoastalCA"
+unique(NA_subsite_level1$year) #2002-2021
+unique(NA_subsite_level1$subsite_level2)#"SCI_YELLOWBANKS_W" "HORSESHOE_REEF_E"  "HORSESHOE_REEF_W" 
+unique(NA_subsite_level1$subsite_level3) #"1" "2" "3" "4"
+
+### lets look at regular CoastalCA setup - i.e., why are these missing
+CoastalCA_test <- dt_wide2 |> 
+  filter(project == "CoastalCA") |> 
+  dplyr::select(site, subsite_level1, subsite_level2, subsite_level3) |> 
+  distinct() #should be mpa or reference
+
+### id mpa vs reference using - https://caseagrant.ucsd.edu/sites/default/files/27B_Caselle-appendicies.pdf
+# "SCI_YELLOWBANKS_W" = reference
+# "HORSESHOE_REEF_E" = reference
+# "HORSESHOE_REEF_W" = reference
+
+### replace nas here with "reference"
+dt_wide3 <- dt_wide2 |> 
+  mutate(subsite_level1 = if_else(project == "CoastalCA" & is.na(subsite_level1), "reference", subsite_level1))
+
+CoastalCA_check <- dt_wide3 |> 
+  filter(project == "CoastalCA") |> 
+  dplyr::select(site, subsite_level1, subsite_level2, subsite_level3) |> 
+  distinct()
+
+### check nas
+na_count_per_column <- sapply(dt_wide3, function(x) sum(is.na(x)))
+print(na_count_per_column)
+
+### tidy environment
+all_objects <- ls() #list all objects in the environment
+object_to_keep <- "dt_wide3" #specify the object you want to keep
+rm(list = all_objects[all_objects != object_to_keep])
+rm(all_objects, object_to_keep)
+
+#### export and write to the drive
+# Export locally
+tidy_filename <- "harmonized_consumer_excretion_CLEAN.csv"
+
+write.csv(dt_wide3, file = file.path("tier2", tidy_filename), na = '.', row.names = F)
+
+# Export harmonized clean dataset to Drive
+googledrive::drive_upload(media= file.path("tier2",tidy_filename), overwrite = T,
+                          path = googledrive::as_id("https://drive.google.com/drive/u/1/folders/1VakpcnFVckAYNggv_zNyfDRfkcGTjZxX"))
+
---
+++ step6_summary_stats_for_analysis_and_plotting.R
@@ -0,0 +1,139 @@
+###project: LTER Marine Consumer Nutrient Dynamic Synthesis Working Group
+###author(s): Mack White, Li Kui, Angel Chen
+###goal(s): calculate summary stats needed for analysis/figures
+###date(s): January - March 2024
+###note(s): 
+
+# Housekeeping ------------------------------------------------------------
+
+### load necessary libraries
+### install.packages("librarian")
+librarian::shelf(tidyverse, googledrive, readxl, taxize, stringr)
+
+### set google drive path
+exc_ids <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1VakpcnFVckAYNggv_zNyfDRfkcGTjZxX")) |> 
+  dplyr::filter(name %in% c("harmonized_consumer_excretion_CLEAN.csv"))
+
+strata_ids <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/1/folders/1CEgNtAnk4DuPNpR3lJN9IqpjWq0cM8F4")) %>%
+  dplyr::filter(name %in% c("strata_class.xlsx"))
+
+# Combine file IDs
+harmonized_ids <- rbind(exc_ids, strata_ids)
+
+# For each raw data file, download it into the consumer folder
+for(k in 1:nrow(harmonized_ids)){
+  
+  # Download file (but silence how chatty this function is)
+  googledrive::with_drive_quiet(
+    googledrive::drive_download(file = harmonized_ids[k, ]$id, overwrite = T,
+                                path = file.path("tier2", harmonized_ids[k, ]$name)) )
+  
+  # Print success message
+  message("Downloaded file ", k, " of ", nrow(harmonized_ids))
+}
+
+rm(list = ls()) #cleans env
+
+### read in clean excretion and strata data from google drive
+dt <- read.csv(file.path("tier2", "harmonized_consumer_excretion_CLEAN.csv"),stringsAsFactors = F,na.strings =".") |> 
+  janitor::clean_names()
+glimpse(dt)
+
+strata_list <- readxl::read_excel(path = file.path("tier2", "strata_class.xlsx"),na=".") |> 
+  dplyr::select(-...9,-...10)
+glimpse(strata_list)
+
+# set up data for summary statistics --------------------------------------
+
+### replace NAs in subsite_level2 and subsite_level3 columns with "Not Available"
+### to allow group_by function to go as far in sequence as it can for 
+### each project without throwing NAs
+
+dt <- dt %>%
+  mutate(subsite_level2 = replace_na(subsite_level2, "Not Available"),
+         subsite_level3 = replace_na(subsite_level3, "Not Available"))
+
+### check to see NA fixes incorporated
+# na_count_per_column <- sapply(dt, function(x) sum(is.na(x)))
+# print(na_count_per_column)
+
+# summarize data for plotting ---------------------------------------------
+glimpse(dt)
+
+dt_calcs <- dt |> 
+  group_by(project, habitat, year, month, site, subsite_level1, subsite_level2, subsite_level3) |> 
+  summarise(total_n_ug_hr_m = sum(nind_ug_hr * density_num_m, na.rm = TRUE),
+            total_n_ug_hr_m2 = sum(nind_ug_hr * density_num_m2, na.rm = TRUE),
+            total_n_ug_hr_m3 = sum(nind_ug_hr * density_num_m3, na.rm = TRUE),
+            total_n = sum(total_n_ug_hr_m + total_n_ug_hr_m2 + total_n_ug_hr_m3, na.rm = TRUE),
+            # n_ind_sd = sd(nind_ug_hr, na.rm = TRUE),
+            n_ind_sd_nozeros = sd(nind_ug_hr[nind_ug_hr != 0], na.rm = TRUE),
+            # n_ind_mean = mean(nind_ug_hr, na.rm = TRUE),
+            n_ind_mean_nozeros = mean(nind_ug_hr[nind_ug_hr != 0], na.rm = TRUE),
+            # n_ind_cv = (n_ind_sd/n_ind_mean)*100,
+            n_ind_cv_nozeros = (n_ind_sd_nozeros/n_ind_mean_nozeros)*100,
+            total_p_ug_hr_m = sum(pind_ug_hr * density_num_m, na.rm = TRUE),
+            total_p_ug_hr_m2 = sum(pind_ug_hr * density_num_m2, na.rm = TRUE),
+            total_p_ug_hr_m3 = sum(pind_ug_hr * density_num_m3, na.rm = TRUE),
+            total_p = sum(total_p_ug_hr_m + total_p_ug_hr_m2 + total_p_ug_hr_m3, na.rm = TRUE),
+            # p_ind_sd = sd(pind_ug_hr, na.rm = TRUE),
+            p_ind_sd_nozeros = sd(pind_ug_hr[pind_ug_hr != 0], na.rm = TRUE),
+            # p_ind_mean = mean(pind_ug_hr, na.rm = TRUE),
+            p_ind_mean_nozeros = mean(pind_ug_hr[pind_ug_hr != 0], na.rm = TRUE),
+            # p_ind_cv = (p_ind_sd/p_ind_mean)*100,
+            p_ind_cv_nozeros = (p_ind_sd_nozeros/p_ind_mean_nozeros)*100,
+            total_bm_m = sum(dmperind_g_ind*density_num_m),
+            total_bm_m2 = sum(dmperind_g_ind*density_num_m2),
+            total_bm_m3 = sum(dmperind_g_ind*density_num_m3),
+            total_bm = total_bm_m + total_bm_m2 + total_bm_m3,
+            # bm_ind_sd = sd(dmperind_g_ind, na.rm = TRUE),
+            bm_ind_sd_nozeros = sd(dmperind_g_ind[dmperind_g_ind != 0], na.rm = TRUE),
+            # bm_ind_mean = mean(dmperind_g_ind, na.rm = TRUE),
+            bm_ind_mean_nozeros = mean(dmperind_g_ind[dmperind_g_ind != 0], na.rm = TRUE),
+            # bm_ind_cv = (bm_ind_sd/bm_ind_mean)*100,
+            bm_ind_cv_nozeros = (bm_ind_sd_nozeros/bm_ind_mean_nozeros)*100,
+            n_spp = n_distinct(scientific_name[dmperind_g_ind != 0]),
+            n_obs = n(),
+            diet_algae_detritus_n = sum(diet_cat == "algae_detritus", na.rm = TRUE),
+            diet_invert_n = sum(diet_cat == "invert", na.rm = TRUE),
+            diet_algae_invert_n = sum(diet_cat == "algae_invert", na.rm = TRUE),
+            diet_fish_invert_n = sum(diet_cat == "fish_invert", na.rm = TRUE),
+            diet_fish_n = sum(diet_cat == "fish", na.rm = TRUE),
+            prop_algae_detritus = diet_algae_detritus_n / n_obs,
+            prop_invert = diet_invert_n / n_obs,
+            prop_algae_invert = diet_algae_invert_n / n_obs,
+            prop_fish_invert = diet_fish_invert_n / n_obs,
+            prop_fish = diet_fish_n / n_obs) |>
+  ungroup() |> 
+  dplyr::select(-total_n_ug_hr_m, -total_n_ug_hr_m2, -total_n_ug_hr_m3,
+                -total_p_ug_hr_m, -total_p_ug_hr_m2, -total_p_ug_hr_m3,
+                # -total_bm_m, -total_bm_m2, -total_bm_m2, -total_bm_m3,
+                -n_obs,
+                -diet_algae_detritus_n, -diet_invert_n, -diet_algae_invert_n,
+                -diet_fish_invert_n, -diet_fish_n)
+
+### check for NAs 
+na_count_per_column <- sapply(dt_calcs, function(x) sum(is.na(x)))
+print(na_count_per_column) #total_bm missing 15318 obs (that's everything)
+
+### calculate mean total
+bm_avg_test <- dt_calcs |> 
+  group_by(project, habitat) |> 
+  summarize(bm1 = mean(total_bm_m),
+            bm2 = mean(total_bm_m2),
+            bm3 = mean(total_bm_m3),
+            total_bm = mean(total_bm)) 
+#appears that NGA is missing all biomass estimates
+#causing all of the sites to lose their total_bm estimate
+
+number_test <- dt_calcs |> 
+  filter(is.na())
+
+nga_test <- dt |> 
+  filter(project == "NGA")
+glimpse(nga_test)
+
+na_count_per_column <- sapply(nga_test, function(x) sum(is.na(x)))
+print(na_count_per_column)
+
+
