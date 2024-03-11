c6a5069eb3171f5fcbe60141df86393b487de25e
mackwhite <mwhite@fiu.edu>
2024-03-11 14:54:20 -0400

    generated script for figure two revisions

---
+++ scripts_figs/figure_two.R
@@ -0,0 +1,23 @@
+###project: LTER Marine Consumer Nutrient Dynamic Synthesis Working Group
+###author(s): Mack White, Li Kui, Angel Chen
+###goal(s): generate figure two for manuscript
+###date(s): January - March 2024
+###note(s): 
+
+# Housekeeping ------------------------------------------------------------
+
+### load necessary libraries
+### install.packages("librarian")
+librarian::shelf(tidyverse, googledrive, readxl, taxize, stringr, gridExtra, 
+                 MASS, ggrepel, purrr)
+
+### set google drive path
+exc_ids <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1VakpcnFVckAYNggv_zNyfDRfkcGTjZxX")) |> 
+  dplyr::filter(name %in% c("harmonized_consumer_excretion_CLEAN_summarized.csv"))
+
+rm(list = ls()) #cleans env
+
+### read in data from google drive
+dt <- read.csv(file.path("tier2", "harmonized_consumer_excretion_CLEAN_summarized.csv"),stringsAsFactors = F,na.strings =".") |> 
+  mutate(sdate = as.Date(sdate))
+glimpse(dt)
